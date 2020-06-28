a77aeaed9b5e376ab51b16aa3b686517f76c2274
reflective-partial-8357 <scarter31191@gmail.com>
2020-06-28 23:41:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -20,8 +20,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
- holiday_hash[:winter].each do |holiday, supply| holiday_hash [:winter] [holiday] << supply
-  end
+ holiday_hash[:winter].each {|x, i| holiday_hash [:winter] [x] << supply}
 end
 
 
